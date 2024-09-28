bef25e0fa08ff9f649c20586f22297c17bb27c97
CryptoSOL123 <163804105+CryptoSOL123@users.noreply.github.com>
2024-09-27 20:55:26 -0700

    Fixed BowAngle Added Arrow

--- Assets/Scripts/BowAngle.cs
+++ Assets/Scripts/BowAngle.cs
@@ -1,27 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BowAngle : MonoBehaviour
 {
     public GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
+        // Get the mouse position in screen space and convert it to world space
         Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = Camera.main.nearClipPlane; // Ensure correct Z depth
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
+        // Calculate direction from the player to the mouse position
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
 
-       Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
-        transform.up = direction;
+        // Rotate the bow to face the mouse direction with a 90-degree offset
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90); // Subtract 90 degrees to rotate right
 
+        // Set the bow position relative to the player
         transform.position = player.transform.position;
-        
     }
 }
